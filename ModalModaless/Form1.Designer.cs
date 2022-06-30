02c825ff1e294dd9871723465ca77aae9024587f
chaewon0228 <s2033@e-mirim.hs.kr>
2022-06-30 14:46:40 +0900

    #1 메시지박스, 모달, 모달창, MDI 폼 작성

--- ModalModaless/Form1.Designer.cs
+++ ModalModaless/Form1.Designer.cs
@@ -6,7 +6,6 @@
         /// 필수 디자이너 변수입니다.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
         /// <summary>
         /// 사용 중인 모든 리소스를 정리합니다.
         /// </summary>
@@ -19,9 +18,7 @@
             }
             base.Dispose(disposing);
         }
-
         #region Windows Form 디자이너에서 생성한 코드
-
         /// <summary>
         /// 디자이너 지원에 필요한 메서드입니다. 
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
@@ -38,63 +35,69 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(2, 3);
+            this.button1.Location = new System.Drawing.Point(11, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 76);
+            this.button1.Size = new System.Drawing.Size(122, 70);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "내용만";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(122, 3);
+            this.button2.Location = new System.Drawing.Point(139, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(114, 76);
+            this.button2.Size = new System.Drawing.Size(122, 70);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "내용+제목";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(242, 3);
+            this.button3.Location = new System.Drawing.Point(267, 12);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(114, 76);
+            this.button3.Size = new System.Drawing.Size(122, 70);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "내용+제목+버튼";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(362, 3);
+            this.button4.Location = new System.Drawing.Point(396, 13);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(114, 76);
+            this.button4.Size = new System.Drawing.Size(139, 69);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
+            this.button4.Text = "내용+제목+버튼+아이콘";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(2, 85);
+            this.button5.Location = new System.Drawing.Point(13, 99);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(234, 95);
+            this.button5.Size = new System.Drawing.Size(242, 142);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
+            this.button5.Text = "Modal";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(242, 85);
+            this.button6.Location = new System.Drawing.Point(267, 102);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(234, 95);
+            this.button6.Size = new System.Drawing.Size(268, 138);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
+            this.button6.Text = "Modeless";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 393);
+            this.ClientSize = new System.Drawing.Size(547, 258);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
